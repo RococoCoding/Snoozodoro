af412bf16dd812b379a69c8c6ede881b0ce96fbd
Alice Chang <sinsofangels@gmail.com>
2020-10-22 17:44:44 -0400

    Implement auto switch from work to break

--- src/components/timer.js
+++ src/components/timer.js
@@ -3,46 +3,77 @@ import Prefs from "./preferences";
  //countdown timer from stackoverflow: https://stackoverflow.com/questions/60972542/i-want-to-react-usestate-or-other-hooks-implement-a-60-second-countdown-how
 
 export default function Timer (props) {
-  const [workInterval, setWorkInterval] = useState(5);
-  const [breakInterval, setBreaktInterval] = useState(3);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const [isOn, setIsOn] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(5); //mesasured in seconds
-  const [showPrefs, setShowPrefs] = useState(false);
-  const [intervalType, setIntervalType] = useState("work");
-  
+  const [timerOn, setTimerOn] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(5); //mesasured in seconds 
+  const [snoozeOn, setSnoozeOn] = useState(false);
+  const [intervalType, setIntervalType] = useState("Work");
+
+  const workInterval = 5;
+  const breakInterval = 4;
+  const snoozeInterval = 3;
+  const showPrefs = false;
+  const snoozeCounter = 0;
   const sound = new Audio("https://raw.githubusercontent.com/RococoCoding/asset_hosting/master/Honk1.mp3");
 
   function toggleOn() {
     if (!timeLeft) {
       setTimeLeft(workInterval);
     }
-    setIsOn(!isOn);
+    setTimerOn(!timerOn);
   }
 
   function timerStop() {
-    setIsOn(false);
+    setTimerOn(false);
     setTimeLeft(workInterval);
   }
 
   function loadPrefs() {
-    setShowPrefs(!showPrefs);
+    showPrefs = !showPrefs;
   }
 
+  function submitPrefs(){//preferences form submit
+
+  }
+  useEffect(()=> {
+    if (intervalType === "Work") {
+      setTimeLeft(workInterval);
+    }
+    else if (intervalType === "Break") {
+      setTimeLeft(breakInterval);
+    }
+    else setTimeLeft(snoozeInterval);
+  }, [intervalType]);
+
   useEffect(() => {  //timer countdown
-    if (isOn) {
-      if(timeLeft === 0){
-        setTimeLeft(workInterval);  
-        sound.play();
+    if (timerOn) {
+      if(timeLeft === 0){ //interval type switch -- work, break or snooze
+        if (intervalType === "Work" && snoozeOn) {
+          setIntervalType("Snooze");
+          sound.play();
+        }
+        else if (intervalType === "Work") {
+          setIntervalType("Break");  
+          sound.play();
+        }
+        else if (intervalType === "Break") {
+          setIntervalType("Work");
+          sound.play();
+        }
+        else {
+          setIntervalType("Break");
+          sound.play();
+        }
+        
       }
       const everySecond = setInterval(() => {
         setTimeLeft(timeLeft - 1);
       }, 1000);
       return () => clearInterval(everySecond);
     }
-  }, [isOn, timeLeft]);
+  }, [timerOn, timeLeft]);
 
   useEffect(()=>{ //converts timeLeft to hr/min/sec for display
     let minutesLeft = -1
@@ -66,14 +97,15 @@ export default function Timer (props) {
   <div>
     <div>
       <p>{hours < 10 ? `0${hours}`: hours}:{minutes < 10 ? `0${minutes}`: minutes}:{seconds < 10 ? `0${seconds}`: seconds}</p>
+      <p>{intervalType}</p>
     </div>
-    <button onClick={toggleOn}>{isOn ? "Pause" : "Start"}</button>
+    <button onClick={toggleOn}>{timerOn ? "Pause" : "Start"}</button>
     <button onClick={timerStop}>Stop</button>
     <button onClick={loadPrefs}>Preferences</button>
     {showPrefs 
       && 
-      <Prefs 
-        setWorkInterval={setWorkInterval}
+      <Prefs
+        submit={submitPrefs}
       />}
   </div>)
 }
